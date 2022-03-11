b6d4b9b76fa9409fc9daaa077106cd8450624afd
Weslley Prado <weslley.prado73@gmail.com>
2022-03-11 17:55:08 -0300

    Recebendo e respondendo um input

--- myAppInput/App.js
+++ myAppInput/App.js
@@ -1,35 +1,60 @@
 import React, { Component } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Image, TextInput} from 'react-native';
-
+import { View, Text, StyleSheet, TextInput } from 'react-native'; 
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name: ''
+    };
+
+    this.captureName = this.captureName.bind(this);
+  }
+
+  captureName(text){
+    if(text.length > 0){
+      this.setState({name: 'Welcome to: ' + text});
+    }else{
+      this.setState({name: ''})
+    }
+  }
+
   render(){
     return(
+      <View style={styles.container}>
 
-    <View style={styles.container}>
-   <TextInput 
-   style={style.input}
-   placeholder='Write your name'
-   onChange={this.captureName}
-   />
-    
-  </View>
+        <TextInput
+        style={styles.input}
+        placeholder="Digite seu nome?"
+        underlineColorAndroid="transparent"
+        onChangeText={this.captureName}
+        />
+
+        <Text style={styles.texto}>{this.state.name}</Text>
+
+      </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
   },
   input:{
-    height:45,
-    borderWidth:1,
-    borderColor:'#222'
-    }
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#222',
+    margin: 10,
+    fontSize: 20,
+    padding: 10,
+  },
+  texto:{
+    textAlign: 'center',
+    fontSize: 25
+  }
 });
+
+
 
 export default App;
